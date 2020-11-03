4b2fbb70639867405870d2f0d4addd0d5b0febfc
dana <danaaljamal94@gmail.com>
2020-11-03 14:41:08 -0500

    Initial commit

---
+++ app/src/main/java/com/example/paypark/ui/home/HomeFragment.kt
@@ -0,0 +1,29 @@
+package com.example.paypark.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.paypark.R
+
+class HomeFragment : Fragment() {
+
+    private val TAG = this.toString()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+
+
+
+
+        return root
+    }
+}
